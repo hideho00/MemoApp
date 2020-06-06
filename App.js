5603deea1c8bfb13511b048a8ef603f0dfd2c3da
hideho00 <hideho00@gmail.com>
2020-06-06 20:03:54 +0900

    Refact-2

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
 import Appbar from './src/components/appbar';
 import MemoList from './src/components/MemoList';
-import CircleBotton from './src/elements/CircleButton'
+import CircleButton from './src/elements/CircleButton'
 
 export default class App extends React.Component {
-  render(){
+  render() {
     return (
       <View style={styles.container}>
         <Appbar />
@@ -16,14 +17,12 @@ export default class App extends React.Component {
   }
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFDF6',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 70,
-    shadowColor: '#000'
+    paddingTop: 78,
   },
 });
--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -1,39 +1,39 @@
 import React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
-class CircleBotton extends React.Component {
-    render(){
-        return(){
-            <View style={styles.CircleBotton}>
-                <Text style={styles.CircleBottonTitle}>
-                    {this.props.children}
-                </Text>
-            </View>
-        };
-    }
-} 
+class CircleButton extends React.Component {
+  render() {
+    return (
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonTitle}>
+          {this.props.children}
+        </Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-    CircleBotton: {
-        position: 'absolute',
-        bottom: 32,
-        right: 32,
-        width: 40,
-        height: 40,
-        backgroundColor: '#E31676',
-        borderRadius: 24,
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {width: 0, height: 2 },
-        shadowOpacity: 0.5,
-        shadowRadius: 3,
-    },
-    CircleBottonTitle: {
-        fontSize: 24,
-        width: 20,
-        height: 20,
-    },
+  circleButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  circleButtonTitle: {
+    fontSize: 32,
+    lineHeight: 32,
+    color: '#fff',
+  },
 });
 
-export default CircleBotton;
+export default CircleButton;
